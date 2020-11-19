f9abaed328d15bc35ac7efa928e5508b4e487963
Dilshad Shibin <49745082+dilshad-shibin@users.noreply.github.com>
2020-11-19 18:15:31 +0530

    team section created

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,5 +34,36 @@ $(document).ready(function(){
         }
     });
 
+    if($(".swiper-container").hasClass("team-member-slider")){
+        var swiper = new Swiper('.swiper-container',{
+            slidesPerView:3,
+            allowTouchMove: true,
+            loop: true,
+            centredSlides: true,
+            slideToclickedslide: true,
+            effect: "coverflow",
+            grabcursor:true,
+            autoplay: false,
+            navigation:{
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            coverflow: {
+                rotate: 0,
+                stretch:100,
+                depth:200,
+                modifier: 1,
+                slideShadows: false
+            },
+            breakpoints: {
+                767: {
+                    slidesPerview: 1,
+                    centeredSlides: false,
+                    effect: "slide"
+                }
+            }
+        })
+    };
+
 
 })
